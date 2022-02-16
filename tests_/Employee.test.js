e833999bc43d12e966c1576e9f8c88223dbcad22
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-16 18:27:44 -0500

    employee test

--- tests_/Employee.test.js
+++ tests_/Employee.test.js
@@ -1,3 +1,16 @@
-const Employee = require('/Users/mitchellroutman/repository/homework/team-profile-gen-MR/src/Employee')
+const Employee = require("../src/Employee");
 
-test('test that constrctor values are ')
+test("Can create a new object when you call new Employee", () => {
+    const testVal =  new Employee();
+    expect(typeof(testVal)).toBe("object")
+})
+
+test("when I pass the first parameter on new Employee, I create an id property with my new parameter", () => {
+    const testVal =  new Employee(123);
+    expect(testVal.id).toBe(123)
+})
+
+test("when I pass the second parameter on new Employee, I create an name property with my new parameter", () => {
+    const testVal =  new Employee();
+    expect(testVal.name).toBe(name)
+})
