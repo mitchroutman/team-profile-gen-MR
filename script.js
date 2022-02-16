0ccb9ae032594ba867ee3436d7e1e33d78f29930
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-16 18:04:17 -0500

    revised script with functioning inquirer

--- script.js
+++ script.js
@@ -1,35 +1,143 @@
 //console.log("Hello world");
-const fs = require('fs');
 const inquirer = require('inquirer');
 const Manager = require('/Users/mitchellroutman/repository/homework/team-profile-gen-MR/src/Manager');
 const Engineer = require('/Users/mitchellroutman/repository/homework/team-profile-gen-MR/src/Engineer');
 const Intern = require('/Users/mitchellroutman/repository/homework/team-profile-gen-MR/src/Intern');
 const Employee = require('/Users/mitchellroutman/repository/homework/team-profile-gen-MR/src/Employee');
+const genreateHTML = require('./src/htmlgenerator');
 const Team = [];
 //
 
 //questions manager
-const mainMenu = () => {
-    return inquirer.prompt([
+function mainMenu() {
+
+    inquirer.prompt([
         {
             type: 'list',
             name: 'selector',
-            message: 'What kind of employeed do you want to add?',
+            message: 'What kind of employee do you want to add?',
             choices: ['Manager', 'Engineer', 'Intern']
-        }])
-        .then(userSelect => {
-            switch (userSelect.menu) {
-                case "Manager":
-                    promptManager();
-                    break;
-                case "Engineer":
-                    promptEngineer();
-                    break;
-                case "Intern":
-                    promptIntern();
-                    break;
-                default:
-                    makeTeam();
-            }
+        }]).then(({selector}) => {
+            if(selector === "Manager") {
+                newManager()
+            } else if (selector === "Engineer") {
+                newEngineer()
+            } else (newInter)
         })
+    }
+
+function addMore () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            choices: ['Yes', 'No'],
+            name: 'choice',
+            message: 'Would you like to add more team members?',
+        }]).then(({choice}) => {
+            if(choice === "Yes") {
+                mainMenu()
+            } else {
+                createNewFile()
+                    const htmlPage = generateHTML(Team);
+
+                    FileSystem.writeFile('index.html', htmlPage, (error) =>
+                    error ? console.log(error) : console.log('Generated HTML Team page')
+                    );
+                }
+            }
+        )
+    
 }
+
+
+const newManager = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your managers name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your managers employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your managers email?'
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is your managers office number?'
+        },
+    ]).then(response => {
+        const manager = new Manager(response.id, response.name, response.email, response.office)
+        Team.push(manager);
+        console.log(Team)
+        // console.log(genreateHTML(Team))
+        addMore();
+    })
+}
+
+
+const newEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your Engineers name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your Engineers employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your Engineers email?'
+        },
+        {   type: 'input',
+            name: 'git',
+            message: 'What is your Engineers GitHub?'
+        }
+    ]).then(response => {
+        const engineer = new Engineer(response.id, response.name, respose.email, respose.officenumber)
+        Team.push(engineer);
+        mainMenu();
+    })
+}
+
+const newIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your Interns name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your Interns employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your Interns email?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is your Interns school?'
+        }
+    ]).then(response => {
+        const intern = new Engineer(response.id, response.name, respose.email, respose.officenumber)
+        Team.push(intern);
+        mainMenu();
+    })
+}
+
+mainMenu()
+
